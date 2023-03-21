522031f89cccae6c6f7f0984bb0ff43b5517dec0
Gabriela Paschoal <45124512+gabrielabjn@users.noreply.github.com>
2023-03-21 17:28:15 -0300

    Add files via upload

---
+++ gabi-est066-21-03-23.R
@@ -0,0 +1,84 @@
+# EST066
+# 21/03/23
+# Aplicando a funcao triangular vista em aula.
+
+p_tri <- function(x){
+  
+  if (x<=0)
+    return(0)
+  
+  if (x>0 && x<=1)
+    return (x)
+  
+  if (x>1 && x<=2)
+  
+  if (x>2)
+    return (0)
+  
+}
+
+d_tri <- function(x){
+  
+  if (x<=0)
+    return(0)
+  
+  if (x>0 && x<=1)
+    return(power(x,2)/2)
+  
+  if (x>1 && x<2)
+    return(power(x,2)/2 + 2*x -1)
+    
+  if (x>=2)
+      return(1)
+
+  
+  
+}
+
+q_tri<- function(x){
+  
+  if (x<=0)
+    return(0)
+  
+  if (x>0 && x<=1/2)
+    return (power(2*x,1/2))
+  
+  if (x>1/2 && x<1)
+    return(2-(power(2*(1-x),1/2)))
+  
+  else
+    print("entrada invalida em q_tri")
+  
+}
+
+# abaixo: funcao criada pelo professor pprofessorara gerar amostras aleatorias da funcao 
+# triangular (equivalente a funcao rtri do pacote {EnvStats})
+
+rtri.pri<-function(){ 
+  
+  U<-runif(1)
+  y<-qtri(u)
+  
+  return(y)
+  
+  
+}
+
+
+# gerar 1000 numeros aleatorios da funcao triangular
+
+# fazer o histograma (densidade) dos 1000 numeros aleatorios.
+
+# plotar a densidade da triangular no histograma (use 'curve')
+
+# calcular: media dos 1000 numeros aleatorios proporcao < 0.5
+
+# ptri.vec <- vectorize(ptri)
+
+# ptri.vec(c(0,0.5,1,1.5,2))
+# gerados<-replicate(1000,rtri.pri())
+
+
+
+
+
